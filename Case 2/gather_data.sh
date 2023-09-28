6e241efcba4016e4de7a2c7e8096eebc33365133
Rafael de Bem <debemrafael@gmail.com>
2023-09-28 15:33:59 -0300

    wip

---
+++ Case 2/gather_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+replicas=(1 5 10 20 50 100)
+
+for amount in ${replicas[@]}; do
+    echo starting $amount client containers
+
+    export CLIENT_REPLICAS=$amount
+    docker compose up -d
+    SECONDS=0
+    while (( SECONDS < 125 )); do
+        docker stats --no-stream | cat >> logs_${amount}.csv
+    done
+    docker compose down
+done
+
+echo -e "\a"
